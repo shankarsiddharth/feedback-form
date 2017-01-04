86c2e398bdf77314cb0de9c2939255c1946e927e
Shankar Siddharth K A <shankarsiddharthka@gmail.com>
2017-01-04 07:51:03 +0530

    add index.js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $("#load").click(function(){
+        $.get("https://shankarsiddharth.github.io/sample_data.json", function(data, status){
+        //alert("Data 1: " + data.review1 + "\nData 2: " + data.review2 + "\nStatus: " + status);
+        if(status == 'success'){
+            $("#review1").text(data.review1);
+            $("#review2").text(data.review2);
+        }else{
+            console.log("GET request failed.");
+        }
+    });
+    });
+    //http://requestb.in/1a969ou1
+    $("#submit").click(function(){
+        var review1 = $("#review1").val();
+        var review2 = $("#review2").val();
+        //alert("Data 1: " + review1 + "\nData 2: " + review2);
+        var review = {
+            "review1": review1,
+            "review2": review2
+        };
+        $.post("http://requestb.in/1a969ou1", review ,function(response, status){
+            console.log("Success");
+            alert("Status: " + status);        
+    });
+    });
+});
